881f65059f6536ae7df6c82ccab83fa0ffc3e65f
yvnan2.19 <yveskouadio111@yahoo.fr>
2019-10-28 21:36:01 +0000

    premier commit

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+/* <- Preloader -> */
+$(window).on('load',function(){
+    $('#status').fadeOut();
+    $('#preloader').delay(500).fadeOut('slow');
+    $('body').delay(1000).css({'overflow':'visible'})
+})
+
+$(document).ready(function(){
+    /*<- js slick ->*/
+    $('#slider-cars').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {slidesToShow: 4}
+            }, 
+            {
+            breakpoint: 520,
+            settings: {slidesToShow: 3}
+            },
+            {
+                breakpoint: 377,
+                settings: {slidesToShow:1}
+            },
+            {
+                breakpoint: 415,
+                settings: {slidesToShow:1}
+            }
+        ]
+    });
+    /****************************************/
+
+    /*<-typed js->*/
+    let typed = new Typed('.type',{
+        strings: 
+            ['BIENVENUE CHEZ LUXURY AUTO DEAL'
+            ],
+            typeSpeed:200,
+            backSpeed:200,
+            smartBackspace: true,
+            loop:true
+        });
+    let type = new Typed('.type2',{
+        strings: 
+            ['LE MEILLEUR SERVICE EN LIGNE DE VENTE ET EXPOSITION DE VOITURE...'
+                
+            ],
+            typeSpeed:160,
+            backSpeed:160,
+            loop:false,
+        });
+});
+
